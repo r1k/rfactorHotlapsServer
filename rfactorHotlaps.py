0cfa74d3e869f8fb5cf737291a19959c3804ce10
Richard Kelsey <r.i.kelsey@gmail.com>
2013-01-09 22:46:56 +0000

    basic test application for app engine

---
+++ rfactorHotlaps.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import webapp2
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        
+        self.response.out.write('<html><body>\n')
+        
+        self.response.out.write(template.render('template_html/main.html',{}))
+        self.response.out.write(template.render('template_html/javascript_decl.html',{}))
+
+        self.response.out.write('</body></html>')
+
+if __name__ == "__main__":
+
+    application = webapp2.WSGIApplication([
+                                          ('/', MainPage)
+                                          ], debug=True)    
+    run_wsgi_app(application)
