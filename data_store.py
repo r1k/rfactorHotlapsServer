e97936fc4594eea5065919b06fc25daf83b6f3b0
Richard Kelsey <r.i.kelsey@gmail.com>
2013-01-17 15:18:39 +0000

    data model dev

--- data_store.py
+++ data_store.py
@@ -4,10 +4,19 @@ import datetime
 
 from google.appengine.ext import db
 
+# https://developers.google.com/appengine/docs/python/gettingstartedpython27/usingdatastore
+
+def laptime_key(track_name=None):
+  """Constructs a Datastore key for a lap_record entity with guestbook_name."""
+  return db.Key.from_path('TrackRecords', track_name or 'track')
+
 class lap_record( model.db ):
+	date = db.DateTimeProperty(auto_now_add=True)
 
-	pass
+	driver = db.StringProperty()
+	track = db.StringProperty()
 
-class track_record( model.db ):
+	first_sector = db.TimeProperty()
+	second_sector = db.TimeProperty()
+	total_time = db.TimeProperty()
 
-	pass
