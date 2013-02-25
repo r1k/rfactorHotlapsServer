3a81e129dcd4c9095d1ed4ba7270b65f2f8c94fb
Richard Kelsey <r.i.kelsey@gmail.com>
2013-02-25 23:33:09 +0000

    admin handler and functions

---
+++ admin_handler.py
@@ -0,0 +1,33 @@
+import logging
+from google.appengine.ext.webapp import template
+
+import handler
+
+# handler class for the admin functions
+
+
+class handler(handler.hdlr):
+
+    def __init__(self, request=None, response=None):
+        super(handler, self).__init__(request=request, response=response)
+
+    def get(self, url_ext):
+
+        self.check_for_root()
+
+        logging.debug("admin_handler")
+        logging.debug(url_ext)
+        page_txt = "Admin"
+
+        content = template.render('template_html/branding_bar.html', {'page': page_txt})
+
+        if url_ext.startswith('lap_insert'):
+            logging.debug("lap_insert")
+            content += self.lap_insert()
+            logging.debug(content)
+
+        logging.debug("done")
+        self.render(content)
+
+    def lap_insert(self):
+        return template.render('template_html/admin_lap_insert.html', {})
