2d8c1028dc5cbc5755cb1eb81cf4dfc4b85a21e2
Richard Kelsey <r.i.kelsey@gmail.com>
2013-03-04 09:18:12 +0000

    Make fastest lap lists work

--- charts_handler.py
+++ charts_handler.py
@@ -13,21 +13,27 @@ def genResultList(l_name, t_name, c_name):
                 [sup.lap_result.from_lap_record(l) for l in laps])
 
 
-def genClassList(l_name, t_name, c_name, base_url):
+def genFastestResultList(l_name, t_name, c_name):
+    laps = data_store.interface(l_name).get_fastest_lap_times(t_name, c_name)
+    return sup.calculate_lap_diffs(
+                [sup.lap_result.from_lap_record(l.lap.get()) for l in laps])
+
+
+def genClassList(l_name, t_name, c_name, base_url, list_gen=genFastestResultList):
     template_params = {'trackname': t_name,
                        'carclassname': c_name,
                        'track_url': base_url + t_name,
                        'track_clas_url': base_url + t_name + '/' + c_name}
-    template_params['result_list'] = genResultList(l_name, t_name, c_name)
+    template_params['result_list'] = list_gen(l_name, t_name, c_name)
     return template.render('template_html/track_result.html', template_params)
 
 
-def genTrackList(l_name, t_entity, base_url):
+def genTrackList(l_name, t_entity, base_url, list_gen=genFastestResultList):
     t_name = t_entity.get_name()
     cclasses = data_store.carclass(t_entity)
     html = ""
     for c_name in cclasses.get_all_names():
-        html += genClassList(l_name, t_name, c_name, base_url)
+        html += genClassList(l_name, t_name, c_name, base_url, list_gen)
     return html
 
 
@@ -35,12 +41,13 @@ class handler(handler.hdlr):
 
     def __init__(self, request=None, response=None):
         super(handler, self).__init__(request=request, response=response)
+        logging.getLogger().setLevel(logging.INFO)
 
     def get(self, url_ext):
         self.check_for_root()
 
         logging.debug("charts_handler")
-        logging.debug(url_ext)
+        logging.debug("Params: %s", url_ext)
         content = template.render('template_html/branding_bar.html',
                                   {'page': "Charts"})
 
@@ -54,20 +61,26 @@ class handler(handler.hdlr):
         tracks_if = data_store.tracks(l_name)
 
         num_args = len(url_extras)
+        logging.debug("Num args: " + str(num_args) + ', list: ' + str(url_extras))
         if num_args == 0:
             #generate html containing top times for each track found
-            logging.info('list all')
+            logging.debug('list all')
+            t_list = tracks_if.get_all()
+            logging.debug('track list: ' + str(t_list))
             for t_entity in tracks_if.get_all():
+                logging.debug("track: " + t_entity.get_name())
                 content += genTrackList(l_name,
                                         t_entity,
-                                        base_url='./charts/')
+                                        base_url='./charts/',
+                                        list_gen=genFastestResultList)
 
         elif num_args == 1:
             #generate list of lap times for a specific track
             logging.info('list by track')
             content += genTrackList(l_name,
                                     tracks_if.get_by_name(url_extras[0]),
-                                    base_url='')
+                                    base_url='',
+                                    list_gen=genFastestResultList)
 
         elif num_args == 2:
             #list all times for a specific track and car class
@@ -75,7 +88,8 @@ class handler(handler.hdlr):
             content += genClassList(l_name,
                                     url_extras[0],
                                     url_extras[1],
-                                    base_url='../')
+                                    base_url='../',
+                                    list_gen=genResultList)
 
         elif num_args == 3:
             #list all the times for a specific track, car class, and driver
